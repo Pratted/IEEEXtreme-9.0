ead769d72ae195462fbda9cdd682e926f863857d
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 10:58:45 +0300

    done

--- 10. zoom in/zoom_in.py
+++ 10. zoom in/zoom_in.py
@@ -11,18 +11,39 @@ translations = {}
 for _ in range(number_of_translations):
     character = raw_input()
 
-    translation = ""
-    # translation = []
+    translation = []
     for _ in range(rows):
-        translation += raw_input()
-        translation += "\n"
+        translation.append(raw_input())
 
     translations[character] = translation
 
 
 # print(translations)
 
-pp.pprint(translations)
+# pp.pprint(translations)
 
-for element in translations:
-    print(translations[element])
+#for element in translations:
+#    print(translations[element])
+
+number_of_strings = int(raw_input())
+
+output = ["" for _ in range(rows)]
+# print(output)
+
+for _ in range(number_of_strings):
+    string = raw_input()
+
+    for character in string:
+        translation = translations[character]
+
+        for i in range(rows):
+            output[i] += translation[i]
+
+
+# print(output)
+
+output_string = ""
+for line in output:
+    output_string += line + "\n"
+
+print(output_string.strip())
