842d5be9f4975ebd72cbc90ea63860c534c554c9
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 04:58:10 +0300

    cleanup

--- 3. block art/block_art.py
+++ 3. block art/block_art.py
@@ -6,21 +6,13 @@ rows, columns = dimensions.split(" ")
 rows = int(rows)
 columns = int(columns)
 
-# print("rows: ", rows)
-# print("columns: ", columns)
-
 number_of_queries = int(raw_input())
-# print("number_of_queries: ", number_of_queries)
 
 queries = []
 for _ in range(number_of_queries):
     queries.append(raw_input())
 
-# print("queries: ", queries)
-
 matrix = [[0 for _ in range(columns)] for _ in range(rows)]
-# print("matrix:")
-# print(matrix)
 
 for query in queries:
     elements = query.split(" ")
@@ -29,12 +21,6 @@ for query in queries:
     top_left_column = int(elements[2])
     bottom_right_row = int(elements[3])
     bottom_right_column = int(elements[4])
-
-    # print("operation: ", operation)
-    # print("top_left_row: ", top_left_row)
-    # print("top_left_column: ", top_left_column)
-    # print("bottom_right_row: ", bottom_right_row)
-    # print("bottom_right_column: ", bottom_right_column)
 
     cubes = 0
     for row in range(top_left_row - 1, bottom_right_row):
