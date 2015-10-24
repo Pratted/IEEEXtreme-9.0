0bc8329c6f4063c8f7c5c270067a0d95af00394b
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 04:16:33 +0300

    cleanup

--- 1. digit fun/digit_fun.py
+++ 1. digit fun/digit_fun.py
@@ -7,31 +7,15 @@ while line != "END":
     line = raw_input()
 
 
-# print(lines)
-
-# results = []
-
 for line in lines:
-    # print("line: ", line)
-
     if line != "END":
         alphas = []
 
         number = int(line)
-        # print(number)
         alphas.append(number)
         alphas.append(len(line))
 
         while alphas[-1] != alphas[-2]:
-            # print(alphas)
-            # print(len(alphas) - 1)
-            # print
-
             alphas.append(len(str(alphas[-1])))
 
-        # print(alphas)
-        # print("the smallest positive i of {} is {}".format(alphas[0], alphas[-1]))
         print(len(alphas) - 1)
-        # print
-
-        # results.append(len(alphas) - 1)
