2db4694b4ca3ab0281e86f5310d0128e274bfef4
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 19:37:26 +0300

    done

--- 16. dictionary strings/dictionary_strings.py
+++ 16. dictionary strings/dictionary_strings.py
@@ -39,13 +39,25 @@ for potential_dictionary in potential_dictionaries:
     potential_dictionary_list = list(potential_dictionary)
     # print(potential_dictionary_list)
 
+    potential_dictionary_usage = {}
+    for character in potential_dictionary:
+        if character in potential_dictionary_usage:
+            potential_dictionary_usage[character] += 1
+        else:
+            potential_dictionary_usage[character] = 1
+    print("potential_dictionary_usage:")
+    print(potential_dictionary_usage)
+
+    original_potential_dictionary_usage = dict(potential_dictionary_usage)
+
     missing = 0
     for word in words:
-        temp_potential_dictionary = list(potential_dictionary_list)
         # print("")
         # print(word)
         word = list(word)
         # print(word)
+
+        temp_potential_dictionary = list(potential_dictionary_list)
 
         temp_word = list(word)
         for character in temp_word:
@@ -53,8 +65,13 @@ for potential_dictionary in potential_dictionaries:
             if character in temp_potential_dictionary:
                 word.remove(character)
                 temp_potential_dictionary.remove(character)
+                potential_dictionary_usage[character] += -1
             else:
                 missing += 1
+
+        for character in potential_dictionary_usage:
+            if potential_dictionary_usage[character] > 0:
+                potential_dictionary_usage[character] = original_potential_dictionary_usage[character]
 
         # print(temp_potential_dictionary)
         # print("missing: {}".format(missing))
@@ -64,6 +81,17 @@ for potential_dictionary in potential_dictionaries:
     else:
         state[potential_dictionary][0] = "No"
         state[potential_dictionary][1] = missing
+
+    print("potential_dictionary_usage:")
+    print(potential_dictionary_usage)
+    print("")
+
+    if any(potential_dictionary_usage[key] > 0 for key in potential_dictionary_usage):
+        if state[potential_dictionary][0] != "No":
+            state[potential_dictionary][1] = "No"
+    else:
+        if state[potential_dictionary][0] != "No":
+            state[potential_dictionary][1] = "Yes"
 
 
 print("state:")
