c469e0f03482fdd62700e04c75f1a7e1b4935b05
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 21:24:38 +0300

    some code

--- 24. that's so characteristically euler/that's_so_characteristically_euler.py
+++ 24. that's so characteristically euler/that's_so_characteristically_euler.py
@@ -1,6 +1,17 @@
 from __future__ import print_function
+from pprint import pprint
 
 number_of_test_cases = int(raw_input())
+
+def number_matrix(matrix):
+    for row in matrix:
+        for cell in row:
+            if cell == "X":
+                cell = 1
+            else:
+                cell = 0
+    return matrix
+
 
 for _ in range(number_of_test_cases):
     # height of the image
@@ -12,7 +23,17 @@ for _ in range(number_of_test_cases):
     pattern = []
     for _ in range(n):
         line = raw_input()
-        pattern.append(line)
-    print("pattern:")
-    print(pattern)
+        pattern.append(list(line))
+    # print("pattern:")
+    pprint(pattern)
     print("")
+
+    #pattern = number_matrix(pattern)
+    #pprint(pattern)
+    #print("")
+
+    for i in range(m):
+        for j in range(n):
+            print(pattern[i][j])
+            if pattern[i][j] == "X":
+                on_region = True
