afe081e1d6d4388de8eab0d3f8fc5278c3ec3040
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 20:05:56 +0300

    import

---
+++ 24. that's so characteristically euler/that's_so_characteristically_euler.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+
+# height of the image
+n = raw_input()
+
+# width of the image
+m = raw_input()
+
+cases = []
+for _ in range(n):
+    line = raw_input()
+    cases.append(line)
+print("cases:")
+print(cases)
+print("")
