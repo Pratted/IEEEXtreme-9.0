c4c973c16ef5f790e485c36eae3ea5e65a6b0ddd
Dimitrios Paraschas <paraschas@gmail.com>
2015-10-24 06:09:14 +0300

    numpy optimization, still fails

--- 3. block art/block_art.py
+++ 3. block art/block_art.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import numpy as np
 
 dimensions = raw_input()
 
@@ -12,9 +13,11 @@ queries = []
 for _ in range(number_of_queries):
     queries.append(raw_input())
 
-matrix = [[0 for _ in range(columns)] for _ in range(rows)]
+# matrix = [[0 for _ in range(columns)] for _ in range(rows)]
+matrix = np.zeros((rows, columns))
 
 for query in queries:
+    # print("query: " + query)
     elements = query.split(" ")
     operation = elements[0]
     top_left_row = int(elements[1])
@@ -22,15 +25,24 @@ for query in queries:
     bottom_right_row = int(elements[3])
     bottom_right_column = int(elements[4])
 
-    cubes = 0
-    for row in range(top_left_row - 1, bottom_right_row):
-        for column in range(top_left_column - 1, bottom_right_column):
-            if operation == "a":
-                matrix[row][column] += 1
-            elif operation == "r":
-                matrix[row][column] += -1
-            elif operation == "q":
-                cubes += matrix[row][column]
+    if operation == "a":
+        # matrix[top_left_row-1:bottom_right_row][top_left_column-1:bottom_right_column] += 1
+        matrix[top_left_row - 1:bottom_right_row, top_left_column - 1:bottom_right_column] += 1
+    elif operation == "r":
+        # matrix[top_left_row-1:bottom_right_row][top_left_column-1:bottom_right_column] += -1
+        matrix[top_left_row - 1:bottom_right_row, top_left_column - 1:bottom_right_column] += -1
+    elif operation == "q":
+        #cubes = 0
+        #for row in range(top_left_row - 1, bottom_right_row):
+        #   for column in range(top_left_column - 1, bottom_right_column):
+        #       cubes += matrix[row][column]
+
+        cubes = sum(sum(matrix[top_left_row - 1:bottom_right_row, top_left_column - 1:bottom_right_column]))
+        # sum(matrix)
+        # cubes = sum(sum(matrix))
+        # print(cubes)
+
+    # print(matrix)
 
     if operation == "q":
-        print(cubes)
+        print(int(cubes))
